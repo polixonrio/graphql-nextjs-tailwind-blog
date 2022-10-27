996cf5b29e1263e66308a13705482bea47d2d0aa
xilihulu <xilihulu@pm.me>
2022-10-27 22:56:56 +0530

    commmmittt

---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const posts = [
+  { title: 'React Testing', excerpt: 'Learn React Testing' },
+  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' }
+]
+
+const Home: NextPage = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+      <Head>
+        <title>Thoughts</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12' >
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'></div>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Home
